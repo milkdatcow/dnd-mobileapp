627c27b3cac07244af561723a30669ab60aac095
Dyl <bowyer008@gmail.com>
2025-06-22 15:20:59 +0100

    updated styling

--- goblin/filters.js
+++ goblin/filters.js
@@ -1,4 +1,4 @@
-const typeOptions =["aberration","beast","celestial","construct","dragon","elementals","fey","fiend","giant","humanoid","monstrosity","ooze","plant","undead"];
+const typeOptions =["aberration","beast","celestial","construct","dragon","elemental","fey","fiend","giant","humanoid","monstrosity","ooze","plant","undead"];
 const crOptions=["0","1/8","1/4","1/2","1","2","3","4","5","6","7","8","9","10","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
 
 const typeSelect = document.getElementById("type-select");
